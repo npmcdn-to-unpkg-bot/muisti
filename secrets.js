24a405a2c5479915aba33ec40125757004d89788
Lauri Haukijärvi <lauri@lauhamedia.fi>
2016-05-26 13:55:09 +0300

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 module.exports = {
-cookieSecret    : 'n#3K63H{Ut~;S*kW',		 +  cookieSecret    : <lisää>,
-sessionSecret   : 'RIPrince',		 +  sessionSecret   : <lisää>,
+cookieSecret    : 'n#3K63H{Ut~;S*kW',
+sessionSecret   : 'RIPrince',
   		  
 mongoConStr     : 'mongodb://muisti:salasana12@ds013310.mlab.com:13310/muisti'
 };
