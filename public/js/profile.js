1ef1c69e863d0e56f04618a36cf88a2e57599294
Lauri Haukijärvi <lauri@lauhamedia.fi>
2016-05-26 14:06:10 +0300

    fix urlify part 2

--- public/js/profile.js
+++ public/js/profile.js
@@ -131,11 +131,11 @@
 
  function urlify(text) {
     var urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.replace(urlRegex, function(url) {
+    /*return text.replace(urlRegex, function(url) {
         return '<a href="' + url + '">' + url + '</a>';
-    })
+    })*/
     // or alternatively
-    // return text.replace(urlRegex, '<a href="$1">$1</a>')
+    return text.replace(urlRegex, '<a href="$1">$1</a>')
 }
 
   function generateEmptyList(){
