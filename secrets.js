e45592b78b37ac42e8c3d6de427854925ce7e65f
Lauri Haukijärvi <lauri@lauhamedia.fi>
2016-07-03 20:02:23 +0300

    Pieni muutos

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 module.exports = {
-cookieSecret    : 'n#3K63H{Ut~;S*kW',
-sessionSecret   : 'RIPrince',
+cookieSecret    : 'TÄHÄN KEKSISALAISUUS',
+sessionSecret   : 'TÄHÄN SESSIOSALAISUUS',
   		  
-mongoConStr     : 'mongodb://muisti:salasana12@ds013310.mlab.com:13310/muisti'
+mongoConStr     : 'CONNECTION STRING TÄHÄN'
 };
